5f4541664979ec97b5b708823d349450edfcce80
sou7hernsaint <inquiries@james-boyette.com>
2022-11-13 21:21:20 -0500

    chore: update css

--- client/src/pages/Explore.js
+++ client/src/pages/Explore.js
@@ -18,22 +18,24 @@ const Explore = props => {
     <div className='city-selector'>
       <canvas width='1296' height='991' style={canvasStyle}></canvas>
       {/* TODO: The container div should be a flex container */}
-      <div className='city-selector-prompt'>Where are you looking for experiences?</div>
-      <div className='city-selector-cities'>
-        <div className='gold'>
-          {/* <img src=''>🗽 New York</img> */}
-          {/* TODO: why are these img tags in the original? */}
-          <span>🗽 New York</span>
-        </div>
-        <div className='gold'>
-          <span>🌴 Miami</span>
-        </div>
-        <div className='gold'>
-          <span>☀️ Los Angeles</span>
-        </div>
-        <div className=''>
-          {/* TODO: The container div should be a flex container */}
-          <span>📍 Near Me</span>
+      <div className='flex-container'>
+        <div className='city-selector-prompt'>Where are you looking for experiences?</div>
+        <div className='city-selector-cities'>
+          <div className='gold'>
+            {/* <img src=''>🗽 New York</img> */}
+            {/* TODO: why are these img tags in the original? */}
+            <span>🗽 New York</span>
+          </div>
+          <div className='gold'>
+            <span>🌴 Miami</span>
+          </div>
+          <div className='gold'>
+            <span>☀️ Los Angeles</span>
+          </div>
+          <div className=''>
+            {/* TODO: The container div should be a flex container */}
+            <span>📍 Near Me</span>
+          </div>
         </div>
       </div>
     </div>
