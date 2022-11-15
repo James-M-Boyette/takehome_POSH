9fd359d2d03de5f17701e26ade880f39301827fe
James Boyette <inquiries@james-boyette.com>
2022-11-14 23:18:54 -0500

    chore: cleanup events page

--- client/src/pages/ExploreEvents.js
+++ client/src/pages/ExploreEvents.js
@@ -13,6 +13,8 @@ const canvasStyle = {
 
 const ExploreEvents = props => {
   // * SCRIPTS
+  // const {state} = useLocation()
+
   // Display "Events"
   const [events, setEvents] = useState(null)
 
@@ -37,11 +39,7 @@ const ExploreEvents = props => {
   // "Back" arrow/button Navigation
   const navigate = useNavigate()
 
-  /**
-   * ! PARAMS
-   */
-
-  // Set the clicked button to 'selected'
+  // Style 'selected' Button
   const updateButton = e => {
     const eventButtons = Array.from(document.getElementsByClassName('event-select'))
     eventButtons.forEach(x => x.classList.remove('selected'))
@@ -49,73 +47,31 @@ const ExploreEvents = props => {
     e.currentTarget.classList.add('selected')
   }
 
-  // TODO Params
-  // const {state} = useLocation()
-  // const {c, t, p, city} = state
-  let params = new URL(document.location).searchParams
-
-  // Search Params
+  // Params
   const [searchParams, setSearchParams] = useSearchParams()
 
-  let newParams = new URLSearchParams(searchParams.toString())
-
-  const c = searchParams.get('c')
-  const t = searchParams.get('t')
-  const p = searchParams.get('p')
-  const city = searchParams.get('city')
-
   // Handle User Time-Range Selection
-  const selectTimeRange = e => {
+  const selectTimeRange = (e, id) => {
     // Style Button
     updateButton(e)
-
     // Store Params
-    const params = {}
+    let newParams = new URLSearchParams(searchParams.toString())
 
     // Update Params
-    // TODO: convert `e.currentTarget.id` to a passed argument of the same string, and remove each button's ID
-    e.currentTarget.id == 'this-week' ? newParams.set('t', 'week') : ''
-    e.currentTarget.id == 'today' ? newParams.set('t', 'today') : ''
-    e.currentTarget.id == 'thanksgiving' ? newParams.set('t', 'thanksgiving') : ''
-    // e.currentTarget.id == 'this-week' ? params.t = 'week' : ''
-    // e.currentTarget.id == 'today' ? params.t = 'today' : ''
-    // e.currentTarget.id == 'thanksgiving' ? params.t = 'thanksgiving' : ''
+    id == 'this-week' ? newParams.set('t', 'week') : ''
+    id == 'today' ? newParams.set('t', 'today') : ''
+    id == 'thanksgiving' ? newParams.set('t', 'thanksgiving') : ''
 
     // Update 'searchParams'
     setSearchParams(newParams.toString())
-    // setSearchParams(params)
 
-    // router.push(`/reports/parcel?` + new URLSearchParams({id: currentId}))
-    // e.currentTarget.id == 'this-week' ? (params.t = 'week') : ''
-    // e.currentTarget.id == 'this-week' ? searchParams.set('t', 'week') : ''
-    e.currentTarget.id == 'this-week' ? searchParams.set('t', 'week') : ''
-    e.currentTarget.id == 'today' ? setSearchParams({c: 'popular', t: 'today', p: '1', city: 'nyc'}) : ''
-    e.currentTarget.id == 'thanksgiving' ? setSearchParams({c: 'popular', t: 'thanksgiving', p: '1', city: 'nyc'}) : ''
-
-    // console.log('state:', state)
-    console.log('Params:', params)
     console.log('searchParams', searchParams)
     console.log('searchParams to string:', searchParams.toString())
   }
 
-  // Temporary console log
-  useEffect(() => {
-    console.log(c, t, p, city)
-  }, [c, t, p, city])
-
   // * TEMPLATE
   return (
     <div>
-      <div>
-        <h1 style={{color: 'white'}}>Test</h1>
-        {c && <p style={{color: 'white'}}>Category: {c}</p>}
-        {t && <p style={{color: 'white'}}>Timeframe: {t}</p>}
-        {p && <p style={{color: 'white'}}>Page: {p}</p>}
-        {city && <p style={{color: 'white'}}>City: {city}</p>}
-        <button onClick={() => setSearchParams({c: 'popular', t: 'week'})}>Change Params</button>
-        {/* Note: you need to set all K:Vs - if you leave 'dog' out, the new query will only have 'food' */}
-        {/* <button onClick={today}>Change Params 2</button> */}
-      </div>
       {/* Event Content */}
       <div className='explore'>
         <video
@@ -137,35 +93,26 @@ const ExploreEvents = props => {
             <div className='explore-body-main-nav'>
               <div className='explore-body-main-nav-right'>
                 {/* <div id="this-week-button" className='selected' onClick={thisWeek}> */}
-                <div id='this-week' className='event-select selected' onClick={selectTimeRange}>
+                <div className='event-select selected' onClick={e => selectTimeRange(e, 'this-week')}>
                   This Week
                 </div>
-                <div id='today' className='event-select' onClick={selectTimeRange}>
+                <div className='event-select' onClick={e => selectTimeRange(e, 'today')}>
                   Today
                 </div>
-                <div id='thanksgiving' className='event-select' onClick={selectTimeRange}>
+                <div className='event-select' onClick={e => selectTimeRange(e, 'thanksgiving')}>
                   🦃 Thanksgiving
                 </div>
               </div>
             </div>
             <div className='explore-body-main-results event-card-grid'>
-              <div className='events'>
-                {events &&
-                  events.map(event => (
-                    <p key={event._id}>{event.name}</p>
-
-                    // NOTE: the first version above is good for initially testing your connection to the back end. Once that's set, switching to a component injection is recommended.
-                    // <EventDetails key={event._id} event={event} /> // Note that, just like in Vue, we need a unique 'key' in order to display a list ...
-                    // Also note that we're declaring a 'workout' prop, and passing _ via 'workout'
-                  ))}
-              </div>
+              <div className='events'>{events && events.map(event => <p key={event._id}>{event.name}</p>)}</div>
             </div>
           </div>
         </div>
       </div>
 
       {/* "Back" Button */}
-      <button onClick={() => navigate(-1)}>
+      <button onClick={() => navigate('/explore')}>
         <img
           src='https://posh-b2.s3.us-east-2.amazonaws.com/left-arrow-in-circular-button-black-symbol.svg'
           className='explore-back'></img>
